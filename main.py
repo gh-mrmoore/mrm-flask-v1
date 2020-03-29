9c889d2e34e18ade44298b6a32173560dcea02a2
mrm <matt.rmoore@gmail.com>
2020-03-28 23:45:58 -0500

    add  gunicorn to venv

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-from flask  import Flask
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return "<h1>Deployed!</h1>"
+
+if __name__ == '__main__':
+    app.config['DEBUG'] = True
+    app.run()
