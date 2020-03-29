7ff4c5e7ef2456cf201e3d27c1d84675450478e9
mrm <matt.rmoore@gmail.com>
2020-03-29 00:24:23 -0500

    expand to use jinja templates

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
+import os
+import jinja2
+
 from flask import Flask
+from flask import render_template, request
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1>Deployed!</h1>"
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.config['DEBUG'] = True
